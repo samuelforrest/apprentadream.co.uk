bfe031ae0eb97d9980082d993b0fdd14ee884088
Samuel Forrest <sam@samuelforrest.me>
2025-09-28 13:53:52 +0100

    commit changes

--- app/page.tsx
+++ app/page.tsx
@@ -1,22 +1,24 @@
-"use client";
+import { LoginForm } from "@/components/join/login-form";
+import Image from "next/image";
 
+export default function JoinPage() {
+    return(
+        <div className="min-h-screen bg-black w-full">
+            <div className="flex min-h-screen w-full justify-center px-4 py-8 md:p-10 md:items-center">
+                <div className="w-full max-w-md space-y-6">
+                    <div className="text-center space-y-4 pt-8 md:pt-0">
+                        <div>
+                           <Image src="/ad.png" alt="logo" width="100" height="50" className="mx-auto"/>
+                        </div>
+                        <h1 className="text-3xl md:text-4xl font-bold text-white">Join Apprentadream</h1>
+                        <p className="text-base md:text-lg text-white/90 px-4 pb-5">Complete the signup form below to gain access to the WhatsApp community instantly.</p>
+                    </div>
+                    <div className="pb-16">
+                        <LoginForm/>
+                    </div>
+                </div>
+            </div>
 
-import About from "@/components/landing-page/about";
-import Hero from "@/components/landing-page/hero";
-import Testimonials from "@/components/landing-page/testimonials";
-
-import ScrollNavbar from "@/components/landing-page/navbar";
-
-
-export default function LandingPage() {
-  return (
-    <div className="relative w-full bg-white">
-      <ScrollNavbar />
-      <section className="relative w-full min-h-[90vh] flex items-center justify-center">
-        <Hero />
-      </section>
-      <About />
-      <Testimonials />
-    </div>
-  )
+        </div>
+    )
 }
--- app/success/page.tsx
+++ app/success/page.tsx
@@ -10,10 +10,10 @@ export default function Success () {
                         <div className="flex min-h-svh w-full items-center justify-center flex-col p-6 md:p-10">
                             <div className="flex-col items-center">
                                 <div>
-                                    <Image src="/ad.png" alt="logo" width="50" height="50" className="mx-auto"/>
-                                </div>*/
+                                    <Image src="/ad.png" alt="logo" width="100" height="50" className="mx-auto"/>
+                                </div>
                                 <p className="text-4xl font-bold text-center p-7 text-white">Welcome to the Apprentadream Community!</p>
-                                <p className="text-lg text-center text-white">Click the link below to join the group chat</p>
+                                <p className="text-lg text-center text-white">Click the link below to join the WhatsApp Community instantly.</p>
                                 <div className="flex justify-center mt-10">
                                     <Link href="/join">
                                         <Button size="lg" className="px-8 py-6 text-lg text-white bg-green-700 rounded-xl cursor-pointer">
